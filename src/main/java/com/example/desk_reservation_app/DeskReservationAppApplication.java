671c468ab11d2ce505a51fec665546b38cfe6078
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-05 19:57:38 +0300

    spring project initialization

---
+++ src/main/java/com/example/desk_reservation_app/DeskReservationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.desk_reservation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskReservationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeskReservationAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/desk_reservation_app/DeskReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desk_reservation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskReservationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
