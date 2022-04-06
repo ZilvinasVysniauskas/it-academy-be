209c9374a686f9c9ef4acb263b339c6520314ff7
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-06 10:56:48 +0300

    testing

---
+++ src/main/java/com/example/desk_reservation_app/controller/HerokuTestingController.java
@@ -0,0 +1,16 @@
+package com.example.desk_reservation_app.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/test")
+public class HerokuTestingController {
+    @GetMapping
+    @CrossOrigin
+    public String  testHerokuServer() {
+        return "working";
+    }
+}
