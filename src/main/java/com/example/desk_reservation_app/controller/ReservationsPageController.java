d4c4241116df0c235497bd8cddf88055b2700c0d
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-11 16:34:08 +0300

    changed to local db

--- src/main/java/com/example/desk_reservation_app/controller/ReservationsPageController.java
+++ src/main/java/com/example/desk_reservation_app/controller/ReservationsPageController.java
@@ -26,6 +26,7 @@ public class ReservationsPageController {
     }
 
     @PostMapping
+    @CrossOrigin
     public void reserveTableOrUpdateReservation(@RequestBody ReservationRequest reservationRequest){
         deskReservationService.reserveTableOrUpdateReservation(reservationRequest);
     }
